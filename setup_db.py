22eddff8c663f2f46c6aa39f57ab9114868903e7
JuanPablo2807 <juan.gonzalez1016alumnos.udg.mx>
2025-10-30 12:57:54 -0600

    Agregue mis cosas

---
+++ setup_db.py
@@ -0,0 +1,25 @@
+import sqlite3
+DB_PATH = 'vulnerable.db'
+
+
+def init_db():
+    conn = sqlite3.connect(DB_PATH)
+    cur = conn.cursor()
+    cur.execute(""""CREATE TABLE IF NOT EXISTS users(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        email TEXT,
+        )""")
+    cur.execute("DELETE FROM users")
+    users = [
+        ('Alice', 'alice@example.com'),
+        ('Bob', 'bob@example.com'),
+        ('Charlie', 'charlie@example.com'),
+        ]
+    cur.executemany("INSERT INTO users (name, email) VALUES (?, ?)", users)
+    conn.commit()
+    conn.close()
+    print("Database initialized with sample data en", DB_PATH)
+    
+if __name__ == '__main__':
+    init_db()
