2e3dd13ae4dcecae0a41434dfaf3eeada8979baf
xcesarg <cc204729@gmail.com>
2025-10-30 12:57:33 -0600

    vulnerabilidad arreglada

--- lab1/app.py
+++ lab1/app.py
@@ -11,19 +11,18 @@ def get_db():
 
 @app.route('/')
 def home():
-    return "Lab 1: API vulnerable a sql injection"
+    return "Lab 1: API corregida (uso de consultas parametrizadas)"
 
 @app.route('/user')
 def get_user():
     name = request.args.get('name','')
     conn = get_db()
     cur = conn.cursor()
-    query = "SELECT id, name, email FROM users WHERE name = '{}';".format(name)
-    cur.execute(query)
+    cur.execute("SELECT id, name, email FROM user WHERE name = ?", (name,))
     rows = cur.fetchall()
     result = [dict(row) for row in rows]
     conn.close()
     return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
